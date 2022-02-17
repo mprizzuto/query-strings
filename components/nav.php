7ab3dcb0fec4dba0ff03e71eb7a10dcd94f6d4ac
Marco R <mprizzuto88@gmail.com>
2022-02-16 19:30:48 -0500

    add looped nav items to nav.php

---
+++ components/nav.php
@@ -0,0 +1,10 @@
+<?php require "../data/nav-data.php";?>
+
+<nav class="site-menu">
+	<?php foreach($navItems as $nav): ?>
+		<?php foreach($nav as $key => $value): ?>
+	    <a href="<?=$value?>"><?=$key?></a>
+	   
+		<?php endforeach; ?>
+	<?php endforeach; ?>
+</nav>
