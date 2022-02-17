f0b4375b059336fcd3672cec935bfef0634e4a8a
Marco R <mprizzuto88@gmail.com>
2022-02-16 19:14:07 -0500

    write formatInput and generateMeta function

--- functions.php
+++ functions.php
@@ -1,8 +1,28 @@
-functons.php
 <?php 
+//an easier to read var_dump()
 function formatInput($input) {
 	echo "<pre>";
 	var_dump($input);
 	echo "<pre>";
 }
+
+// variables
+$page = $_GET["page"] ?? null;
+
+
 ?>
+
+<?php function generateMeta(string $title, string $description) {?>
+	 <head>
+    <link rel="stylesheet" type="text/css" href="./styles/style.css">
+    
+    <meta charset="utf-8">
+
+    <title><?=$title?></title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="description" content="<?=$description?>">
+    
+  </head>
+<?php } ?>
