08c6c1c1ec9fc409582bf9861626c65b18f10d87
Marco R <mprizzuto88@gmail.com>
2022-02-16 22:08:25 -0500

    put data for coffee products/detail page

--- data/coffee-data.php
+++ data/coffee-data.php
@@ -1,1 +1,35 @@
-coffee-data.php
+<?php 
+$coffeeBrands = [
+  [
+  	"brand" => "golden brew",
+  	"photo" => "./images/placeholder.png",
+  	"price" => 20.00,
+  	"teaser" => "the goldenst of brews",
+    "link" => "?page=coffee-detail&golden"
+  ],
+  [
+	  "brand" => "blackheart coffee",
+		"photo" => "./images/placeholder.png",
+		"price" => 35.00,
+		"teaser" => "coffee as black as your heart",
+    "link" => "?page=coffee-detail&blackheart"
+  ],
+  [
+	  "brand" => "tic brew",
+		"photo" => "./images/placeholder.png",
+		"price" => 25.00,
+		"teaser" => "coffee as black as your heart",
+    "link" => "?page=coffee-detail&blackheart"
+  ],
+
+];
+
+/*
+  [
+	  "brand" => "",
+		"photo" => "",
+		"price" => "",
+		"teaser" => "",
+  ]
+*/ 
+?>
---
+++ data/coffee-details.php
@@ -0,0 +1,36 @@
+<?php 
+$coffeeDetails = [
+  $goldenBrew = [
+  	"brand" => "golden brew",
+	  "quantity" => "40 oz",
+	  "pre-ground" => false,
+	  "sold-out" => true,
+	  "description" => "this is a premium organic coffee from the california gold mines. The miner's actually drank this coffee while prospecting for god during the california gold rush",
+  ],
+  $blackheart = [
+  	"brand" => "blackheart coffee",
+	  "quantity" => "20 oz",
+	  "pre-ground" => true,
+	  "sold-out" => false,
+	  "description" => "this coffee will give make your heart skip a beat, it is that strong",
+  ],
+
+  $ticBrew = [
+		"brand" => "tic brew",
+	  "quantity" => "40 oz",
+	  "pre-ground" => true,
+	  "sold-out" => false,
+	  "description" => "all proceeds from this coffee benefit Tourette's Syndrome research",
+	],
+];
+
+/*
+$variable = [
+	"brand" => "",
+  "quantity" => "",
+  "pre-ground" => bool,
+  "sold-out" => bool,
+  "description" => "",
+],
+*/ 
+?>
